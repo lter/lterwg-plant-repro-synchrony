cb16388a9853cd2c8b43c4c12819c6b03d0de52e
njlyon0 <nicholasjlyon@gmail.com>
2023-11-03 13:36:44 -0400

    Added in the part of the trait levels ANOVA results that go into (what is currently) figure 6

--- synchrony_figures.R
+++ synchrony_figures.R
@@ -315,7 +315,7 @@ shp_palette <- c("AND" = 22, "BNZ" = 21, "CDR" = 24, "CWT" = 23,
 
 # Define objects to keep
 keep_objects <- c("sync_df", "spp_traits", "perm_df", "mrm_results", 
-                  "stat_aov", "aov_cld", "aov_pairs",
+                  "stat_aov", "aov_results", "aov_cld", "aov_pairs",
                   "site_palette", "signif_palette", "shp_palette")
 
 # Clean up  environment
@@ -712,6 +712,210 @@ cowplot::plot_grid(fig5a, fig5bc, labels = c("A", ""), nrow = 2, ncol = 1,
 # Export it!
 ggsave(filename = file.path("synchrony_figure_files", "sync_fig5_MRM_results.png"),
        plot = last_plot(), width = 10, height = 10, units = "in", dpi = 720)
+
+# Clean up  environment
+rm(list = setdiff(ls(), c(keep_objects, "keep_objects")))
+
+## ------------------------------------------ ##
+    # Figure 6 - ANOVAs of Trait 'Levels' ----
+## ------------------------------------------ ##
+
+# Make a dataframe for this figure
+fig6_df <- sync_df %>%
+  # Pare down to needed columns
+  dplyr::select(lter, Species_Pair, r.spearman, dplyr::ends_with("_values")) %>%
+  # Pivot to long format
+  tidyr::pivot_longer(cols = dplyr::ends_with("_values"),
+                      names_to = "trait", values_to = "trait_levels") %>%
+  # Coerce lter to all
+  dplyr::mutate(lter = "All") %>%
+  # Attach ANOVA results
+  dplyr::left_join(aov_results, by = c("lter", "trait")) %>%
+  # Simplify trait entries
+  dplyr::mutate(trait = tolower(gsub(pattern = "_", replacement = " ", x = trait))) %>%
+  # Handle one synonymization issue
+  dplyr::mutate(trait = ifelse(trait == "pollinator code values", 
+                               yes = "pollinator values", no = trait)) %>%
+  # Attach compact letter display content
+  dplyr::left_join(aov_cld, by = c("lter", "trait", "trait_levels")) %>%
+  # Now drop CLDs if the global test was non-significant
+  dplyr::mutate(cld_letters = ifelse(result %in% c("NS", "NA"),
+                                     yes = NA, no = letter)) %>%
+  # Drop original CLD letter column
+  dplyr::select(-letter) %>%
+  # Tweak trait formatting to make cleaner facet labels
+  dplyr::mutate(trait_label = factor(stringr::str_to_title(trait),
+                                     levels = sort(unique(stringr::str_to_title(trait)))), 
+                .before = trait) %>%
+  # Tidy up / simplify trait levels to make x-axis tick marks as simple as possible
+  dplyr::mutate(trait_levels = dplyr::case_when(
+    ## Dispersal syndrome
+    trait_levels == "abiotic-endozoochory" ~ "abio.-endozo.",
+    trait_levels == "abiotic-abiotic" ~ "abiotic",
+    trait_levels == "abiotic-synzoochory" ~ "abio.-synzo.",
+    trait_levels == "endozoochory-endozoochory" ~ "endozo.",
+    trait_levels == "endozoochory-synzoochory" ~ "endozo.-synzo.",
+    trait_levels == "synzoochory-synzoochory" ~ "synzo.",
+    ## Mycorrhiza
+    trait_levels == "am-am" ~ "AM",
+    trait_levels == "am-em" ~ "AM-EM",
+    trait_levels == "am-ericoid" ~ "AM-ericoid",
+    trait_levels == "am-none" ~ "AM-none",
+    trait_levels == "em-em" ~ "EM",
+    trait_levels == "em-ericoid" ~ "EM-ericoid",
+    trait_levels == "ericoid-ericoid" ~ "ericoid",
+    ## Pollinators
+    trait_levels == "animal-animal" ~ "animal",
+    trait_levels == "wind-wind" ~ "wind",
+    # trait_levels == "animal-wind" ~ "",
+    ## Deciduous vs. evergreen
+    trait_levels == "deciduous-deciduous" ~ "deciduous",
+    trait_levels == "deciduous-evergreen" ~ "decid.-everg.",
+    trait_levels == "evergreen-evergreen" ~ "evergreen",
+    ## Sexual system
+    trait_levels == "dioecious-dioecious" ~ "dioec.",
+    trait_levels == "dioecious-hermaphrodite" ~ "dioec.-hermaph.",
+    trait_levels == "dioecious-monoecious" ~ "dioec.-monoec.",
+    trait_levels == "dioecious-polygamo-dioecious" ~ "dioec.-polyg. dioec.",
+    trait_levels == "hermaphrodite-hermaphrodite" ~ "hermaph.",
+    trait_levels == "hermaphrodite-monoecious" ~ "hermaph.-monoec.",
+    trait_levels == "hermaphrodite-polygamo-dioecious" ~ "hermaph.-polyg. dioec.",
+    trait_levels == "monoecious-monoecious" ~ "monoec.",
+    trait_levels == "monoecious-polygamo-dioecious" ~ "monoec.-polyg. dioec.",
+    trait_levels == "polygamo-dioecious-polygamo-dioecious" ~ "polygamo dioec.",
+    ## Shade tolerance
+    trait_levels == "intermediate-intermediate" ~ "mid.",
+    trait_levels == "intermediate-intolerant" ~ "mid.-intoler.",
+    trait_levels == "intermediate-tolerant" ~ "mid.-toler.",
+    trait_levels == "intolerant-intolerant" ~ "intolerant",
+    trait_levels == "intolerant-tolerant" ~ "intoler.-toler.",
+    trait_levels == "tolerant-tolerant" ~ "tolerant",
+    ## Growth form
+    trait_levels == "liana-liana" ~ "liana",
+    # trait_levels == "liana-shrub" ~ "",
+    # trait_levels == "liana-tree" ~ "",
+    trait_levels == "shrub-shrub" ~ "shrub",
+    # trait_levels == "shrub-tree" ~ "",
+    trait_levels == "tree-tree" ~ "tree",
+    ## Seed development
+    trait_levels == "1-1" ~ "1",
+    trait_levels == "2-2" ~ "2",
+    trait_levels == "3-3" ~ "3",
+    ## Seed bank
+    trait_levels == "no-no" ~ "no",
+    # trait_levels == "no-yes" ~ "",
+    trait_levels == "yes-yes" ~ "yes",
+    # If isn't "fixed" above, keep as-is
+    TRUE ~ trait_levels)) %>% 
+  # Filter to only desired traits
+  dplyr::filter(trait %in% c("pollinator values", "deciduous evergreen values"))
+
+# Check it out
+dplyr::glimpse(fig6_df)
+
+# Specify order (left to right) of trait levels across all traits
+level_vec <- c(
+  ## Pollinators
+  "animal", "wind", "animal-wind",
+  ## Seed development
+  "1", "2", "3", "2-3",
+  ## Mycorrhiza
+  "AM", "EM", "ericoid", "AM-EM", "AM-ericoid", "AM-none", "EM-ericoid",
+  ## Deciduous vs. Evergreen
+  "deciduous", "evergreen", "decid.-everg.",
+  ## Dispersal
+  "abiotic", "endozo.", "synzo.", "abio.-endozo.", "abio.-synzo.", "endozo.-synzo.",
+  ## Sexual system
+  "dioec.", "monoec.", "hermaph.", "polygamo dioec.", "dioec.-hermaph.", 
+  "dioec.-monoec.", "dioec.-polyg. dioec.", "hermaph.-monoec.", 
+  "hermaph.-polyg. dioec.", "monoec.-polyg. dioec.",
+  ## Shade tolerance
+  "intolerant", "mid.", "tolerant", "intoler.-toler.", "mid.-intoler.", "mid.-toler.",
+  ## Growth form
+  "liana", "shrub", "tree", "liana-shrub", "liana-tree", "shrub-tree",
+  ## Fleshy fruit / seed bank
+  "yes", "no", "no-yes", "yes-no")
+
+# Make an empty list for storing per-trait graphs
+fig6_plotlist <- list()
+
+# Loop across traits
+for(aov_trait in unique(fig6_df$trait_label)){
+  # for(aov_trait in "Pollinator Values") {
+  
+  # Subset data to only that trait
+  fig6_subdf <- fig6_df %>%
+    dplyr::filter(trait_label == aov_trait)
+  
+  # Get an averaged dataframe too
+  fig6_avgsubdf <- supportR::summary_table(data = fig6_subdf, response = "r.spearman",
+                                           groups = c("trait_label", "result", "trait_levels"))
+  
+  # Make another object that is *only* the compact letter display content
+  fig6_subclddf <- fig6_subdf %>%
+    dplyr::select(trait_levels, cld_letters) %>%
+    dplyr::distinct()
+  
+  # Make this a named vector
+  fig6_subcld <- fig6_subclddf$cld_letters
+  names(fig6_subcld) <- fig6_subclddf$trait_levels
+  
+  # Identify trait levels for this trait  
+  sub_level_vec <- level_vec[level_vec %in% unique(fig6_subdf$trait_levels)]
+  
+  # Make plot
+  fig6_subplot <- ggplot(fig6_subdf, aes(x = trait_levels, y = r.spearman)) +
+    # Add horizontal line at synchrony = 0
+    geom_hline(yintercept = 0, linetype = 3, linewidth = 1) +
+    # Actual plotting content
+    geom_jitter(aes(color = result), width = 0.15, alpha = 0.25) +
+    geom_violin(aes(fill = result), alpha = 0.1) +
+    facet_wrap(. ~ trait_label, ncol = 5) +
+    # Add averaged points with SD bars
+    geom_errorbar(data = fig6_avgsubdf, aes(x = trait_levels, y = mean,
+                                            ymax = mean + std_dev,
+                                            ymin = mean - std_dev), width = 0) +
+    geom_point(data = fig6_avgsubdf, aes(x = trait_levels, y = mean, fill = result),
+               shape = 21, size = 3) +
+    # Add label for global P value
+    ylim(-1, 1.1) +
+    geom_text(label = unique(fig6_subdf$P_label), 
+              x = (length(unique(fig6_subdf$trait_levels)) / 2) + 0.5, 
+              y = -0.975, size = 6) +
+    # Customize formatting / aesthetics
+    labs(x = "Trait Values", y = "Cross-Species Synchrony") +
+    scale_fill_manual(values = signif_palette) +
+    scale_color_manual(values = signif_palette) +
+    scale_x_discrete(limits = sub_level_vec) +
+    supportR::theme_lyon(title_size = 14, text_size = 11) +
+    theme(legend.position = "none",
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          strip.text.x = element_text(size = 11))
+  
+  # Now add CLD letters to that plot (for each level in this trait)
+  if(unique(is.na(fig6_subclddf$cld_letters)) != TRUE){
+    # Loop to be flexible across different number of within-plot trait level numbers
+    for(level_num in 1:length(unique(fig6_subdf$trait_levels))){
+      fig6_subplot <- fig6_subplot +
+        geom_text(label = fig6_subcld[[level_num]], 
+                  x = names(fig6_subcld[level_num]), 
+                  y = 1, angle = 90) } }
+  
+  # Add plot to the list
+  fig6_plotlist[[aov_trait]] <- fig6_subplot
+  
+  # Message
+  message("Graph created for '", aov_trait, "'")
+  
+} # Close figure list
+
+# Generate actual figure
+cowplot::plot_grid(plotlist = fig6_plotlist, nrow = 1, ncol = 2, align = 'h',
+                   labels = rep(x = "", times = length(fig6_plotlist)))
+
+# Save it locally
+ggsave(filename = file.path("synchrony_figure_files", "sync_fig6_ANOVA_results_levels.png"),
+       plot = last_plot(), width = 7, height = 5, units = "in", dpi = 720)
 
 # Clean up  environment
 rm(list = setdiff(ls(), c(keep_objects, "keep_objects")))
