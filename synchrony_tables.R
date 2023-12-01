afb75202d88a848c14d797b7fb31aa80b978e848
angelchen7 <angelchen7777@yahoo.com>
2023-11-30 16:58:35 -0800

    Redid Table S2

--- synchrony_tables.R
+++ synchrony_tables.R
@@ -39,6 +39,11 @@ purrr::walk2(.x = wanted_files$id,
                                                 path = file.path("figure_data", .y),
                                                 overwrite = T))
 
+# Also download the full integrated attributes table (for later)
+googledrive::drive_ls(path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1PGaPAkNz1lmvZQMwwthmS-ZjQ97BS2Am")) %>%
+  dplyr::filter(name == "LTER_integrated_attributes_USDA_2022-12-14.csv") %>%
+  googledrive::drive_download(file = .$id, path = file.path("figure_data",.$name), overwrite = T)
+
 # Make a folder to export tables to
 dir.create(path = file.path("table_data"), showWarnings = F)
 
@@ -188,6 +193,161 @@ for(trait in trait_vals){
             file = file.path("table_data", paste0(trait, "_summary_table.csv")))
   
 }
+
+## ------------------------------------------ ##
+#                Table S2 ----
+## ------------------------------------------ ##
+
+# ------------------------------- Data Prep ------------------------------------
+
+# Read in the pre ordination trait data
+pre_ord_traits <- read.csv(file = file.path("figure_data", "pre_ordination_trait_data.csv")) %>%
+  mutate(Species.Name = gsub("\\."," ", Species.Name)) 
+
+# Read in the full trait data
+all_traits <- read.csv(file = file.path("figure_data", "LTER_integrated_attributes_USDA_2022-12-14.csv")) 
+
+# Grab the exact seed mass values 
+seed_mass <- all_traits %>%
+  dplyr::select(species, Seed_mass_mg)
+
+pre_ord_with_mass <- pre_ord_traits %>%
+  # Left join with our full attributes table for the actual seed mass values
+  dplyr::left_join(seed_mass, by = c("Species.Name" = "species")) %>%
+  # Drop the log seed mass values
+  dplyr::select(-Log10_seed_mass_mg) %>%
+  # Pivot to long format
+  tidyr::pivot_longer(cols = -lter:-Species.Name,
+                      names_to = "trait", values_to = "trait_value") %>%
+  # Separate the name of the trait 
+  tidyr::separate(trait, sep = "__", into = c("trait", "trait_category")) %>%
+  # Drop the rows where the species didn't have the trait 
+  dplyr::filter(trait_value > 0) %>%
+  # Fill in the trait category column with missing seed development and seed mass info 
+  dplyr::mutate(trait_category = dplyr::coalesce(trait_category, as.character(trait_value))) %>%
+  # Drop the trait value column
+  dplyr::select(-trait_value) %>%
+  # Rename the trait category column into the true trait value column
+  dplyr::rename(trait_value = trait_category) %>%
+  # Pivot wider into more columns
+  tidyr::pivot_wider(names_from = trait, values_from = trait_value) %>%
+  # Convert the seed development and seed mass columns into numeric
+  dplyr::mutate(dplyr::across(.cols = c(Seed_development_1_2or3yrs, Seed_mass_mg), .fns = as.numeric)) %>%
+  # Filter to only desired LTERs
+  dplyr::filter(lter %in% c("AND", "BNZ", "CDR", "CWT", "HBR", "LUQ", "SEV"))
+
+# --------------------- Calculating Quantitative Stats -------------------------
+
+# Calculating overall quantitative stats 
+overall_quant_stats <- pre_ord_with_mass %>% 
+  dplyr::select(-lter) %>%
+  dplyr::distinct() %>%
+  get_summary_stats(
+    Seed_mass_mg, 
+    show = c("n", "min", "max", "mean", "sd")) %>%
+  dplyr::rename(num_species = n) %>%
+  dplyr::mutate(site = "All data")
+
+# Listing the sites we want
+sites_we_want <- c("AND", "BNZ", "CDR", "CWT", "HBR", "LUQ", "SEV")
+
+site_quant_stats <- list()
+
+# Calculating quantitative stats for each individual site
+for (site in sites_we_want){
+  some_stats <-  pre_ord_with_mass %>% 
+    dplyr::filter(lter == site) %>%
+    get_summary_stats(
+      Seed_mass_mg, 
+      show = c("n", "min", "max", "mean", "sd"))
+  
+  site_quant_stats[[site]] <- some_stats
+  
+}
+
+# Adding a 'site' column and combining all the site dataframes into one dataframe
+site_quant_stats_v2 <- site_quant_stats %>%
+  purrr::imap(.f = ~mutate(.x, site = paste0(.y), .before = everything())) %>%
+  purrr::map_dfr(.f = select, everything()) %>%
+  dplyr::rename(num_species = n)
+
+# Combine the overall and site specific stats together
+quant_stats_altogether <- overall_quant_stats %>%
+  dplyr::bind_rows(site_quant_stats_v2) %>%
+  dplyr::relocate(site, .before = num_species) %>%
+  dplyr::rename(trait = variable)
+
+# Exporting quantitative stats
+write.csv(quant_stats_altogether, file.path("table_data", "quant_trait_stats.csv"), row.names = FALSE)
+
+# --------------------- Calculating Qualitative Stats --------------------------
+
+# Listing the columns we want
+columns_we_want <- c("Pollinator_code", "Mycorrhiza_AM_EM", "Deciduous_Evergreen_yrs",
+                     "Dispersal_syndrome", "Sexual_system", "Shade_tolerance",
+                     "Growth_form", "Fleshy_fruit", "Seed_bank")
+
+overall_qual_stats <- list()
+
+# Calculating overall qualitative stats 
+for (col in columns_we_want){
+  some_stats <- pre_ord_with_mass %>%
+    dplyr::select(-lter) %>%
+    dplyr::distinct() %>%
+    dplyr::group_by(!!sym(col)) %>%
+    dplyr::summarize(count = n()) %>%
+    dplyr::mutate(variable = col, .before = everything()) %>%
+    dplyr::rename(value = !!sym(col)) %>%
+    dplyr::mutate(value = as.character(value))
+  
+  overall_qual_stats[[col]] <- some_stats
+  
+}
+
+# Combining overall qualitative stats into one dataframe
+overall_qual_stats_v2 <- overall_qual_stats %>%
+  purrr::map_dfr(.f = select, everything()) %>%
+  dplyr::rename(num_species = count) %>%
+  dplyr::mutate(site = "All data")
+
+site_qual_stats <- list()
+
+# Calculating qualitative stats for each individual site
+for (site in sites_we_want){
+  for (col in columns_we_want){
+    some_stats <- pre_ord_with_mass %>%
+      dplyr::filter(lter == site) %>%
+      dplyr::group_by(!!sym(col)) %>%
+      dplyr::summarize(count = n()) %>%
+      dplyr::mutate(variable = col, .before = everything()) %>%
+      dplyr::rename(value = !!sym(col)) %>%
+      dplyr::mutate(value = as.character(value))
+    
+    site_qual_stats[[site]][[col]] <- some_stats
+  }
+}
+
+# Adding a 'site' column 
+for (site in sites_we_want){
+  site_qual_stats[[site]] <- site_qual_stats[[site]] %>% 
+    purrr::map_dfr(.f = select, everything()) %>%
+    dplyr::mutate(site = site, .before = everything())
+}
+
+# Combining all the site dataframes into one dataframe
+site_qual_stats_v2 <- site_qual_stats %>%
+  purrr::map_dfr(.f = select, everything()) %>%
+  dplyr::rename(num_species = count) 
+
+# Combine the overall and site specific stats together
+qual_stats_altogether <- overall_qual_stats_v2 %>%
+  dplyr::bind_rows(site_qual_stats_v2) %>%
+  tidyr::pivot_wider(names_from = site, values_from = num_species) %>%
+  dplyr::rename(trait = variable) %>%
+  dplyr::rename(trait_value = value)
+
+# Exporting qualitative stats
+write.csv(qual_stats_altogether, file.path("table_data", "qual_trait_stats.csv"), row.names = FALSE)
 
 ## ------------------------------------------ ##
                   # Export ----
